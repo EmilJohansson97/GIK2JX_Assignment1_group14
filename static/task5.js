45ee5d983e175b47ab24dc278795669bfc39c816
Ekaskikallio <h24elika@du.se>
2026-04-22 10:26:18 +0200

    task.5 ändring

--- static/task5.js
+++ static/task5.js
@@ -1,15 +1,19 @@
 document.getElementById("l5").addEventListener("click", task5);
 
+var fuelLayer = L.geoJSON(fuel, {
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup("<p>" + feature.properties.name + "</p>");
+  },
+});
+
+var fuelClusters = L.markerClusterGroup();
+fuelClusters.addLayer(fuelLayer);
+
 function task5() {
   map.flyTo([59.33278782836633, 18.065093563919522], 10);
-  var fuelLayer = L.geoJSON(fuel, {
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup("<p>" + feature.properties.name + "</p>");
-    },
-  });
-
-  var fuelClusters = L.markerClusterGroup();
-  fuelClusters.addLayer(fuelLayer);
-
-  map.addLayer(fuelClusters);
+  if (map.hasLayer(fuelClusters)) {
+    map.removeLayer(fuelClusters);
+  } else {
+    map.addLayer(fuelClusters);
+  }
 }
